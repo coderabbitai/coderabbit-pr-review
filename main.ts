e19bfc33c707c5dd5ddb29dcc8897c362100714f
nimratcoderabbit <nimrat@coderabbit.ai>
2025-07-15 13:20:34 -0400

    Ast-grep files

---
+++ main.ts
@@ -0,0 +1,14 @@
+var foo = 123
+
+function logStuff(input: any) {
+  console.log("Input is:", input)
+  debugger
+}
+
+function doNothing() {}
+
+const process = (value: any) => {
+  var temp = value
+  console.log("Processing:", temp)
+}
+
